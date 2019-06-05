653e43b801b1ced762be55a61217d1413a27470e
Patrick McCombs <pa.mccombs1@gmail.com>
2019-06-05 12:22:15 +0000

    Done.

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,10 +1,9 @@
 class ClassroomsController < ApplicationController
   def show
     @classroom = Classroom.find(params[:id])
-    @student = @classroom.oldest_student
   end
 
-  def index
-    @classrooms = Classroom.all
-  end
+  #def index
+  #  @classrooms = Classroom.all
+  #end
 end
